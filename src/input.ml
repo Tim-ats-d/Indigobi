533271f1de8b283acb78dbcee28f4c73af7469c0
ArtichautCosmique <leo.grange@icloud.com>
2022-02-18 15:17:40 +0100

    Normalize input strings.

--- src/input.ml
+++ src/input.ml
@@ -1,13 +1,25 @@
 module type S = sig
   val input : unit -> string
   val sensitive : unit -> string
+  val normalize : string -> string
 end
 
 module Default : S = struct
-  let input () =
+  let rec input () =
     print_string "INPUT: ";
     flush stdout;
-    input_line stdin
+    input_line stdin |> normalize
+
+  and normalize input_str =
+    let convert_char str chr =
+      let new_chr =
+        match chr with
+        | 'a' .. 'z' | 'A' .. 'Z' | '0' .. '9' -> Printf.sprintf "%c" chr
+        | _ -> Printf.sprintf "%%%02X" @@ Char.code chr
+      in
+      str ^ new_chr
+    in
+    String.fold_left convert_char "" input_str
 
   let sensitive = input
 end
--- src/input.mli
+++ src/input.mli
@@ -1,6 +1,7 @@
 module type S = sig
   val input : unit -> string
   val sensitive : unit -> string
+  val normalize : string -> string
 end
 
 module Default : S
