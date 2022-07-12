5a485535c92ee52ff479f56a05fa2a922f911018
Tim-ats-d <tim.arnouts@protonmail.com>
2022-07-12 16:39:19 +0200

    Add log system.

---
+++ src/common/log.ml
@@ -0,0 +1,20 @@
+open Logger
+
+let fmt lvl log = Printf.sprintf "%a :: %s\n" Lvl.pp lvl log
+
+module OutChanHandler : HANDLER = struct
+  let level = Lvl.Debug
+  let formatter = fmt
+  let emit = Lwt_io.(write stdout)
+end
+
+module LogFileHandler : HANDLER = struct
+  let level = Lvl.Debug
+  let formatter = fmt
+
+  let emit log =
+    Lwt_io.with_file ~mode:Output "logbook.log" (fun outc ->
+        Lwt_io.write outc log)
+end
+
+include (val Logger.make ~level:Debug [ (module OutChanHandler) ])
