a8a5930b38bfe0bb709cd05e680dc826264a86f9
Tim-ats-d <tim.arnouts@protonmail.com>
2022-07-23 02:48:47 +0200

    Improve safety of accessor.

--- src/common/accessor.ml
+++ src/common/accessor.ml
@@ -28,6 +28,13 @@ let make loc fname =
   (module struct
     open Lwt.Syntax
 
+    let touch_dir_if_non_existant () =
+      let* test = Lwt_unix.file_exists loc in
+      if test then Lwt.return_unit
+      else
+        let* () = Lwt_unix.mkdir loc 0o751 in
+        Log.infof "Create %s" loc
+
     let read () =
       Lwt.catch
         (fun () ->
@@ -36,17 +43,15 @@ let make loc fname =
               Lwt.return_some content))
         (function
           | Unix.Unix_error (ENOENT, _, _) ->
-              let* test = Lwt_unix.file_exists loc in
-              let* () =
-                if test then Lwt.return_unit else Lwt_unix.mkdir loc 0o751
-              in
+              let* () = touch_dir_if_non_existant () in
               let* () =
                 Lwt_io.with_file path ~mode:Output (fun _ ->
-                    Log.infof "Create %s" fname)
+                    Log.infof "Create %s" path)
               in
               Lwt.return_none
           | exn -> Lwt.fail exn)
 
     let write str =
+      let* () = touch_dir_if_non_existant () in
       Lwt_io.with_file path ~mode:Output (fun outc -> Lwt_io.write outc str)
   end : S)
