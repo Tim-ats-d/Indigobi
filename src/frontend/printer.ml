8b1c9d59ed79556e26491a1871d25d326632696e
Tim-ats-d <tim.arnouts@protonmail.com>
2022-10-30 16:23:57 +0100

    Fix crash due to control chars.

--- src/frontend/printer.ml
+++ src/frontend/printer.ml
@@ -5,15 +5,24 @@ module type S = sig
 end
 
 module Default : S = struct
-  let gemini (theme : Theme.t) = function
+  let sanitise =
+    String.map (function '\x00' .. '\x1f' | '\x7f' -> '?' | c -> c)
+
+  let gemini (theme : Theme.t) =
+    let open Notty.Infix in
+    function
     | Gemtext.Text t -> Img.string theme.text t
     | Link { url; name } ->
-        let open Notty.Infix in
         Img.string Attr.empty "=> "
         <|> Img.string theme.link url
         <|> Option.fold name ~none:Img.empty ~some:(fun name ->
                 Img.string Attr.empty " " <|> Img.string theme.link_name name)
-    | Preformat { text; _ } -> Img.string Attr.(fg (gray 4)) text
+    | Preformat { text; _ } ->
+        String.split_on_char '\n' text
+        |> List.map sanitise
+        |> List.fold_left
+             (fun img line -> img <-> Img.string theme.preformat line)
+             Img.empty
     | Heading (`H1, head) -> Img.string theme.h1 head
     | Heading (`H2, head) -> Img.string theme.h2 head
     | Heading (`H3, head) -> Img.string theme.h3 head
